95ea5fd123c08c7fcdc02f70b7cb9d162a980d4d
Anthilam <timtim300@gmail.com>
2017-10-18 13:34:22 +0200

    ajout de googletest

--- testfa.c
+++ testfa.c
@@ -1,16 +1,11 @@
 // BOISSON Romain - GUY Timothée
 
 #include "fa.h"
-#include <CUnit/CUnit.h>
 
 // Test de la fonction fa_create
 void test_fa_create(void) {
 	struct fa a;
 	fa_create(&a, 4, 5);
-
-	CU_ASSERT_TRUE(a.alpha_count == 4);
-	CU_ASSERT_TRUE(a.state_count == 5);
-	CU_ASSERT_FALSE(a.state_array == NULL)
 
 	fa_destroy(&a);
 }
@@ -20,8 +15,6 @@ void test_fa_destroy(void) {
 	struct fa a;
 	fa_create(&a, 4, 5);
 	fa_destroy(&a);
-
-	CU_ASSERT_TRUE(a.state_array == NULL);
 }
 
 // Test de la fonction fa_set_state_initial
